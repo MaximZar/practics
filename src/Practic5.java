8a9c68fa13a34b95ddfd39c17cc548d1bed60f34
Maxim Zadorkin <m.a.zadorkin@gmail.com>
2020-04-24 13:10:45 +0300

    Добавлен выбор практики для вывода

---
+++ src/Practic5.java
@@ -0,0 +1,6 @@
+public class Practic5 {
+  public void Practic5() {
+    System.out.println("Практическая работа №5:");
+    return;
+  }
+}
--- src/Practics.java
+++ src/Practics.java
@@ -1,21 +1,32 @@
+import java.util.Scanner;
 public class Practics {
     public static void main (String args[]) {
-        Practic1 pr1 = new Practic1();
-        pr1.Practic1();
-
-        System.out.println();
-
-        Practic2 pr2 = new Practic2();
-        pr2.Practic2();
-
-        System.out.println();
-
-        Practic3 pr3 = new Practic3();
-        pr3.Practic3();
-
-        System.out.println();
-
-        Practic4 pr4 = new Practic4();
-        pr4.Practic4();
+        Scanner in = new Scanner(System.in);
+        System.out.print("Какую практику вывести? ");
+        int num = in.nextInt();
+        switch (num) {
+            case 1: 
+                Practic1 pr1 = new Practic1();
+                pr1.Practic1();
+                break;
+            case 2:
+                Practic2 pr2 = new Practic2();
+                pr2.Practic2();
+                break;
+            case 3:
+                Practic3 pr3 = new Practic3();
+                pr3.Practic3();
+                break;
+            case 4:
+                Practic4 pr4 = new Practic4();
+                pr4.Practic4();
+                break;
+            case 5:
+                Practic5 pr5 = new Practic5();
+                pr5.Practic5();
+                break;
+            default:
+                System.out.println("Такой практики нет"); 
+        }
     }
 }
